098d529a497e38c693664a0b1a736d8926556ae9
Ariesh Grover <arieshgrover0@gmail.com>
2018-08-28 13:40:28 -0700

    String Exercise

---
+++ Strings/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Hello ";
+            string s2 = "World ";
+            string s3 = "!";
+            s1 += s2 + s3;
+            Console.WriteLine(s1);
+            Console.WriteLine(s1.ToUpper());
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Welcome to McDonald's");
+            sb.Append(" May I take your order");
+            sb.Append(". Thank you for your patientce.");
+            Console.WriteLine(sb);
+            Console.Read();
+        }
+    }
+}
