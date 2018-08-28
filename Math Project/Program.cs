213d36a5ed35ede8e4308d6d74e1fa3e69a656e6
groverari <41345908+groverari@users.noreply.github.com>
2018-08-28 11:03:40 -0700

    Math Project

---
+++ Math Project/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VS_Code
+{
+    class Program
+    {
+        static void Main()
+        {
+         Console.WriteLine("what is your name");
+         string yourName = Console.ReadLine();
+         Console.WriteLine("your name is: "+ yourName);
+
+        }
+    }
+}
+ 
