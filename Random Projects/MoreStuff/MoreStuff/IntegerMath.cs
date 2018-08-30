de52983357bff7a620f4c2667e4c3dbfac326003
Ariesh Grover <arieshgrover0@gmail.com>
2018-08-30 15:21:40 -0700

    MathProject

---
+++ Random Projects/MoreStuff/MoreStuff/IntegerMath.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreStuff
+{
+    class IntegerMath
+    {
+        public int maths(int num1)
+        {
+            return num1 + 43;
+        }
+        public int maths(decimal num1)
+        {
+            return Convert.ToInt32(  num1 * 43);
+        }
+        public int maths(string s1)
+        {
+            return Convert.ToInt32(s1) + 53;
+        }
+    }
+}
---
+++ Random Projects/MoreStuff/MoreStuff/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntegerMath foo = new IntegerMath();
+
+            Console.WriteLine(foo.maths(1));
+            Console.WriteLine(foo.maths(1.67m));
+            Console.WriteLine(foo.maths("3"));
+        }
+    }
+}
