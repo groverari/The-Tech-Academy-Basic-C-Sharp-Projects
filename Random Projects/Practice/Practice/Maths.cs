7095a10491a68fe2f0f4aaa040770d4c19c11f12
Ariesh Grover <arieshgrover0@gmail.com>
2018-08-30 15:43:46 -0700

    maths project

---
+++ Random Projects/Practice/Practice/Maths.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Maths
+    {
+        public int doThis(int num1, int num2 = 4)
+        {
+            return num1 + num2 + 88;
+        }
+
+    }
+}
--- Random Projects/Practice/Practice/Program.cs
+++ Random Projects/Practice/Practice/Program.cs
@@ -10,6 +10,26 @@ namespace Practice
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Input a Number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Maths foo = new Maths();
+            try
+            {
+                
+                Console.WriteLine("Input Another Number. This is optional");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                
+                Console.WriteLine(foo.doThis(num1, num2));
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("The Second Number automatically set to 4");
+                Console.WriteLine(foo.doThis(num1));
+            } 
+            Console.ReadLine();
+
         }
     }
 }
+    
