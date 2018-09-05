b1fef0d13a69fa429a46d4b4a862874dd10d9433
Ariesh Grover <arieshgrover0@gmail.com>
2018-09-05 12:24:19 -0700

    New Interface

---
+++ Random Projects/Basic Password Verifacation/Basic Password Verifacation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Basic_Password_Verifacation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Grover99!";
+            string pattern = @"([0-9]+)([A-Z]+)(.{8,15})([a-z])";
+            Match m = Regex.Match(input, pattern);
+            Console.WriteLine(m.Success);
+            Console.ReadLine();
+        }
+    }
+}
--- Random Projects/Practice2/Practice2/Employee.cs
+++ Random Projects/Practice2/Practice2/Employee.cs
@@ -6,13 +6,17 @@ using System.Threading.Tasks;
 
 namespace Practice2
 {
-    class Employee : Person
+    class Employee : Person, IQuittable
     {
         public int ID { get; set; }
 
         public override void sayName()
         {
             Console.WriteLine("Name: " + FirstName + " "+ LastName);
+        }
+        public void Quit()
+        {
+
         }
 
     }
