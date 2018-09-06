7d6260b59ae7cb656f10428165a57572f41a0480
groverari <41345908+groverari@users.noreply.github.com>
2018-09-06 14:11:21 -0700

    Stuct practice

---
+++ Random Projects/Stucts/Stucts/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Stucts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num = new Number() { Amount = 35.20m };
+            Console.WriteLine(num.Amount);
+        }
+    }
+}
