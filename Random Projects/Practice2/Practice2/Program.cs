af72483a140f9701b9775fe992d6bebc173c6186
Ariesh Grover <arieshgrover0@gmail.com>
2018-09-13 14:12:26 -0700

    var practice

---
+++ Random Projects/Practice2/Practice2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Practice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const decimal bodyTemp = 98.6m;
+            var fever = 104;
+
+        }
+    }
+}
---
+++ Random Projects/Practice2/Practice2/number.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice2
+{
+    class number
+    {
+        public number(int num1, int num2)
+        {
+            Console.WriteLine(num1 + num2);
+
+        }
+        public number(int num1) : this(num1, 0)
+        {
+
+        }
+    }
+}
